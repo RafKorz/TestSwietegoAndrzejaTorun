3a5af1b3c456e30ec0b9e7622575078804c7f279
Rafał Korzeniec <51083650+RafKorz@users.noreply.github.com>
2021-03-27 00:29:59 +0100

    Update TestOfParishOffice.java

--- ŚwiętyAndrzejTest/testOfParishOffice/src/test/java/TestOfParishOffice.java
+++ ŚwiętyAndrzejTest/testOfParishOffice/src/test/java/TestOfParishOffice.java
@@ -3,6 +3,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -23,11 +24,31 @@ public class OfficeTest
     {
         Assert.assertEquals(driver.getTitle(), "Parafia pw. Andrzeja Apostoła w Toruniu");
         driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/section[2]/ul[1]/li[4]/a")).click();
-
         Assert.assertEquals(driver.getTitle(), "Biuro Parafialne");
+        Assert.assertEquals(true, verifyElementPresent(driver, By.xpath("/html/body/div[1]/div[2]/div/section[1]/div/div/p[1]")));
+        Assert.assertEquals(true, verifyElementPresent(driver, By.xpath("/html/body/div[1]/div[2]/div/section[1]/div/div/p[2]/strong")));
+//        Assert.assertEquals(true, verifyElementPresent(driver, By.name("Adres: ul. Watzenrodego 14/13")));
+        Assert.assertEquals(true, verifyElementPresent(driver, By.xpath("/html/body/div[1]/div[2]/div/section[1]/div/div/table/tbody/tr[1]/td[2]")));
+        Assert.assertEquals(true, verifyElementPresent(driver, By.xpath("/html/body/div[1]/div[2]/div/section[1]/div/div/table/tbody/tr[2]/td[2]")));
+        Assert.assertEquals(true, verifyElementPresent(driver, By.xpath("/html/body/div[1]/div[2]/div/section[1]/div/div/table/tbody/tr[3]/td[2]")));
+        Assert.assertEquals(true, verifyElementPresent(driver, By.xpath("/html/body/div[1]/div[2]/div/section[1]/div/div/table/tbody/tr[1]/td[1]")));
+        Assert.assertEquals(true, verifyElementPresent(driver, By.xpath("/html/body/div[1]/div[2]/div/section[1]/div/div/table/tbody/tr[2]/td[1]")));
+        Assert.assertEquals(true, verifyElementPresent(driver, By.xpath("/html/body/div[1]/div[2]/div/section[1]/div/div/table/tbody/tr[3]/td[1]")));
 
+    }
 
-
+    public static boolean verifyElementPresent(WebDriver driver, By by)
+    {
+        try {
+            driver.findElement(by);
+            System.out.println("Element exist");
+            return true;
+        }
+        catch(NoSuchElementException e)
+        {
+            System.out.println("Element not found");
+            return false;
+        }
     }
 
     @After
